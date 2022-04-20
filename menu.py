c4b32ac9175b60b2ec9010b194da6d2f2a3c6992
Mirko Sinnaeve <mirko.sinnaeve@gmail.com>
2022-04-20 14:03:23 +0200

    Camera

--- menu.py
+++ menu.py
@@ -2,41 +2,28 @@ from numpy import size
 import pygame
 import pygame.camera
 import pygame_menu
-# import cv2
+import cv2
 
-# vid = cv2.VideoCapture(0)
-  
-# while(True):
-      
-#     # Capture the video frame
-#     # by frame
-#     ret, frame = vid.read()
-  
-#     # Display the resulting frame
-#     cv2.imshow('frame', frame)
-      
-#     # the 'q' button is set as the
-#     # quitting button you may use any
-#     # desired button of your choice
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-
+cap = cv2.VideoCapture(0) 
 pygame.init()
-pygame.camera.init()
-
-
-
-cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
 size = (600,400)
 
-surface = pygame.display.set_mode((1920, 1280), pygame.RESIZABLE)
+surface = pygame.display.set_mode((780, 540), pygame.RESIZABLE)
 snapshot = pygame.surface.Surface(size, 0, surface)
-menu = pygame_menu.Menu('Welcome', 1540,845,theme=pygame_menu.themes.THEME_SOLARIZED)
+menu = pygame_menu.Menu('Welcome', 780, 540,theme=pygame_menu.themes.THEME_SOLARIZED)
 
-menu.add.button('Camera', cam.start())
+def video():
+    while (True):
+        _, frame = cap.read()
+        cv2.imshow('frame', frame)
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
+
+menu.add.button('Camera', video)
 menu.add.button('New File')
 menu.add.button('Answers')
+menu.add.button('Print Scores', #Functie)
 menu.add.button('Exit', pygame_menu.events.EXIT)
-
 menu.mainloop(surface)
+
