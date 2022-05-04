633f751c27e7a4fd3e42e7df9c41218d9cfb0871
Mirko Sinnaeve <mirko.sinnaeve@gmail.com>
2022-05-04 10:45:28 +0200

    Full menu

--- menu.py
+++ menu.py
@@ -2,31 +2,98 @@ from numpy import size
 import pygame
 import pygame_menu
 import cv2
+from final_sorter import Sorter
+from results_check import ResultsChecker
+from printer import printer
+from machinevision import MachineVision as mv
+import numpy as np
 
-cap = cv2.VideoCapture(0) 
+class Menu:
+    def __init__(self):
+        self.__teacherBlobs = []
+        self.__teacherCircles = []
+        self.__studentBlobs = []
+        self.__studentCircles = []
+
+        self.__answerKey = []
+        self.__studentAnswers = []
+        self.__studentName = "Testing student"
+        self.__grade = 0
+        self.__machineVision = mv()
+        self.__resultsChecker = ResultsChecker(5)
+
+    def updateStudent(self):
+        self.resetScore()
+        self.__resultsChecker.resetMatrixStudent()
+        self.__studentName = input("Please enter the student's name: ")
+        print("Student registered: " + self.__studentName )
+
+    def updateAnswerKey(self):
+        self.__resultsChecker.resetMatrixAnswer()
+        self.resetScore()
+        self.__machineVision.getResults()
+        self.__teacherCircles = self.__machineVision.getCirlces() 
+        self.__teacherBlobs = self.__machineVision.getBlobs()
+        #sorting
+        sorting = Sorter(self.__teacherCircles, self.__teacherBlobs)
+        self.__answerKey = sorting.getSortedAnswerArray(5, 10)
+        print(str(self.__answerKey))
+        self.__resultsChecker.setAnswerKey(self.__answerKey)
+        print(str(self.__resultsChecker.getAnswerKey()))
+        cv2.destroyAllWindows()
+        print("correct answer received")
+
+    def updateStudentAnswers(self):
+        self.__resultsChecker.resetMatrixStudent()
+        print("reset matrix")
+        print(str(self.__resultsChecker.getStudentAnswers()))
+        self.resetScore()
+        self.__machineVision.getResults()
+        self.__studentCircles = self.__machineVision.getCirlces()
+        self.__studentBlobs = self.__machineVision.getBlobs()
+        #sorting
+        sorting = Sorter(self.__studentCircles, self.__studentBlobs)
+        self.__studentAnswers = sorting.getSortedAnswerArray(5, 10)
+        print()
+        print(str(self.__studentAnswers))
+        self.__resultsChecker.setStudentAnswers(self.__studentAnswers)
+        print(str(self.__resultsChecker.getStudentAnswers()))
+        cv2.destroyAllWindows()
+        print("student answer received")
+
+    def gradeGuessCorrection(self):
+        self.__grade = self.__resultsChecker.correctWithGuessCorrection()
+        print("guess correction: " + str(self.__grade))
+
+    def gradeNormalCorrection(self):
+        self.__grade = self.__resultsChecker.correctNormal()
+        print("normal grading: " + str(self.__grade))
+
+    def resetScore(self):
+        self.__grade = 0
+
+    def printScore(self):
+        self.__resultsChecker.printResults("ACM0", self.__studentName)
+
+
 pygame.init()
 size = (600,400)
+bg_color = (4,118,217)
+title_bg_color = (4,66,191)
+text_color = (242,195,167)
+
+
+autograder_theme = pygame_menu.themes.Theme(background_color=bg_color, title_background_color=title_bg_color, widget_font_color=text_color, title_font=pygame_menu.font.FONT_PT_SERIF, widget_font=pygame_menu.font.FONT_HELVETICA, selection_color=text_color)
 
 surface = pygame.display.set_mode((780, 540), pygame.RESIZABLE)
-menu = pygame_menu.Menu('Welcome', 780, 540,theme=pygame_menu.themes.THEME_SOLARIZED)
-help_menu = pygame_menu.Menu('Help', 780, 540, theme=pygame_menu.themes.THEME_SOLARIZED)
+menu = pygame_menu.Menu('Autograder Menu Selection', 780, 540,theme=autograder_theme)
 
-
-
-def video():
-    while (True):
-        _, frame = cap.read()
-        cv2.imshow('frame', frame)
-        k = cv2.waitKey(5) & 0xFF
-        if k == 27:
-            break
-
-menu.add.button('Camera', video)
-menu.add.button('New File')
-menu.add.button('Answers')
-menu.add.button('Print Scores')
-menu.add.button('Help', help_menu)
+mainMenu = Menu()
+menu.add.button('New Student', mainMenu.updateStudent)
+menu.add.button('Upload answer key', mainMenu.updateAnswerKey)
+menu.add.button('Upload student answers', mainMenu.updateStudentAnswers)
+menu.add.button('Grade with guess correction', mainMenu.gradeGuessCorrection)
+menu.add.button('Grade with normal correction', mainMenu.gradeNormalCorrection)
+menu.add.button('Print Scores', mainMenu.printScore)
 menu.add.button('Exit', pygame_menu.events.EXIT)
-
 menu.mainloop(surface)
-
---
+++ results_check.py
@@ -0,0 +1,65 @@
+from printer import printer
+
+class ResultsChecker:
+    def __init__(self,options):
+        self.__totalScore = 0
+        self.__answerKey = 0
+        self.__numberOfQuestions = 0
+        self.__numberOfOptions = options
+        self.__studentAnswers = 0
+
+    def setAnswerKey(self, answerKey):
+        self.__answerKey = answerKey
+        self.__numberOfQuestions = len(self.__answerKey)
+    def getAnswerKey(self):
+        return self.__answerKey
+
+    def setStudentAnswers(self,studentAnswers):
+        self.__studentAnswers = studentAnswers
+    def getStudentAnswers(self):
+        return self.__studentAnswers
+
+    def correctNormal(self):
+        self.__totalScore = 0
+        for i in range(0, self.__numberOfQuestions):
+            if self.__answerKey[i] == self.__studentAnswers[i]:
+               	self.__totalScore += 1
+        return self.__totalScore
+    
+    def correctWithGuessCorrection(self):
+        self.__totalScore = 0
+        point_reduction = (1/(self.__numberOfOptions - 1))
+        for i in range(0, self.__numberOfQuestions):
+            if self.__answerKey[i] == self.__studentAnswers[i]:
+               	self.__totalScore += 1
+            elif self.__studentAnswers[i] == 0:
+                self.__answerKey += 0
+            else:
+                self.__totalScore -= point_reduction
+        if self.__totalScore < 0:
+            self.__totalScore = 0
+
+        return self.__totalScore
+
+    def printResults(self, printerPort, studentName):
+        my_printer = printer.Printer(printerPort)
+        my_printer.print_text("The score for ")
+        my_printer.print_text(studentName)
+        my_printer.print_text(" is: ")
+        my_printer.print_text(str(self.__totalScore) + "/" + str(self.__numberOfQuestions))     
+        print("Printing") 
+        
+
+    def resetScore(self):
+        self.__totalScore = 0
+
+    def resetMatrixStudent(self):
+        self.__studentAnswers = 0
+        
+    def resetMatrixAnswer(self):
+        self.__answerKey = 0
+        self.__numberOfQuestions = 0
+
+
+
+            
